953612e509946a115cdac28d0911e1871d75d1a2
Katie Wokasch <katie.wokasch@name.com>
2026-01-28 06:46:58 -0700

    Update OKR styling; add selector functionality

--- frontend/src/components/okrs/OKRForm.tsx
+++ frontend/src/components/okrs/OKRForm.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState } from 'react';
 import { Button } from '../ui/Button';
 import { Input } from '../ui/Input';
 import { Textarea } from '../ui/Textarea';
